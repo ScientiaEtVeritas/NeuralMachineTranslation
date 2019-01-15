ffcc599197e86aa34206ac04275f632e99d230fe
nicolas-luhn <luhnni@t-online.de>
2019-01-15 16:57:39 +0100

    temp

--- seq2seq.py
+++ seq2seq.py
@@ -5,29 +5,29 @@ import torch.nn.functional as F
 import random
 from LanguageModel import LanguageTokens
 
+class model_config():
+    def __init__(self, input_size, hidden_size, output_size, max_length = 50, rnn_type = 'lstm', bidirectional = False, attention = 'global', dropout_p = 0.1, num_layers_encoder=1, num_layers_decoder=1):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.max_length = max_length
+        self.rnn_type = rnn_type
+        self.bidirectional = bidirectional
+        self.attention = attention
+        self.dropout_p = dropout_p
+        self.num_layers_encoder = num_layers_encoder
+        self.num_layers_decoder = num_layers_decoder
+
+class training_config():
+    def __init__(self, learning_rate = 0.01, teacher_forcing_ratio = 0.5):
+        self.learning_rate = learning_rate
+        self.teacher_forcing_ratio = teacher_forcing_ratio
+
+class prediction_config():
+    def __init__(self, beam_width = 1):
+        self.beam_width = beam_width
+
 class seq2seq():
-    class model_config():
-        def __init__(self, input_size, hidden_size, output_size, max_length = 50, rnn_type = 'lstm', bidirectional = False, attention = 'global', dropout_p = 0.1, num_layers_encoder=1, num_layers_decoder=1):
-            self.input_size = input_size
-            self.hidden_size = hidden_size
-            self.output_size = output_size
-            self.max_length = max_length
-            self.rnn_type = rnn_type
-            self.bidirectional = bidirectional
-            self.attention = attention
-            self.dropout_p = dropout_p
-            self.num_layers_encoder = num_layers_encoder
-            self.num_layers_decoder = num_layers_decoder
-
-    class training_config():
-        def __init__(self, learning_rate = 0.01, teacher_forcing_ratio = 0.5):
-            self.learning_rate = learning_rate
-            self.teacher_forcing_ratio = teacher_forcing_ratio
-
-    class prediction_config():
-        def __init__(self, beam_width = 1):
-            self.beam_width = beam_width
-
     def __init__(self, model_config = None, training_config = None, prediction_config = None, state_dict = None, device = None):
         self.encoder = EncoderRNN(model_config, device = device).to(device)
         self.decoder = DecoderRNN(model_config, device = device).to(device)
