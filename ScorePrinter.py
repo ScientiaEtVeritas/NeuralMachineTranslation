3701c218a4b0ea12ab250ed8ffaa810f3e456491
alma <alma@almaalmaalma.com>
2019-01-20 12:37:17 +0100

    print and store bleu values

--- ScorePrinter.py
+++ ScorePrinter.py
@@ -5,17 +5,20 @@ class ScorePrinter:
         self.count = 0
         self.prefix = prefix
         
-    def update(self, input, output, ground_truth, nll):
+    def update(self, input, output, ground_truth, nll, real_target_sentence, estimated_target_sentence):
         self.count += 1
         for (metric_name, metric_func) in self.metrics:
-            self.scores[metric_name].append(metric_func(input, output, ground_truth, nll))
+            self.scores[metric_name].append(metric_func(input, output, ground_truth, nll, real_target_sentence, estimated_target_sentence))        
             
     def printAvg(self, showCount = True, last = None):
         last = last or self.count
         print(f" \n[{self.prefix}] {str(self.count) + ' examples /' if showCount else ''} ", end = ' ')
         for (metric_name, _) in self.metrics:
             avg_score = sum(self.scores[metric_name][-last:]) / last
-            print("{0}: {1:.2f}".format(metric_name, avg_score), end=' ')
+            if metric_name != "BLEU":
+              print("{0}: {1:.2f}".format(metric_name, avg_score), end=' ')
+            else: 
+              print(" ".join(["BLEU-{0}: {1:.2f}  ".format("weighted" if i==0 else i, float(x)) for i, x in enumerate(avg_score)]))
             
     def startEpoch(self, epoch):
         print(f"\n\nEpoch {epoch} started")
@@ -28,6 +31,6 @@ class ScorePrinter:
     def get_avg_score(self, all=False):
         avg_scores = dict()
         for (metric_name, _) in self.metrics:
-            avg_scores[metric_name] = sum(self.scores[metric_name][-self.count:]) / (self.count)
-        print(f"Self.count : {self.count}")
+            avg_scores[metric_name] = sum(self.scores[metric_name][-self.count:]) / (self.count) if metric_name !='BLEU' else (sum(self.scores[metric_name][-self.count:]) / (self.count)).tolist()
+        #BLEU was a tensor, now it will be a list for faciliting the storage and the visualization
         return avg_scores
