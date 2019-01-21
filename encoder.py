24ad3e7f3abc666f23458745fd339fb71971351e
alma <alma@almaalmaalma.com>
2019-01-21 21:47:29 +0100

    Revert "using hidden state now"
    This reverts commit 5cfa334d089471062828d0fe66f843d0d518bef3.
    
    revert to attention visualization

--- encoder.py
+++ encoder.py
@@ -14,15 +14,16 @@ class EncoderRNN(nn.Module):
         self.embedding = nn.Embedding(model_config.input_size, model_config.hidden_size)
         self.bidirectional = model_config.bidirectional
         self.rnn_type = model_config.rnn_type
-        self.num_layers = model_config.num_layers_encoder
-        self.rnn = rnn_utils.initRNN(model_config.rnn_type, self.hidden_size, self.hidden_size, self.num_layers, bidirectional=self.bidirectional)
+        self.rnn = rnn_utils.initRNN(model_config.rnn_type, self.hidden_size, self.hidden_size, model_config.num_layers_encoder, bidirectional=self.bidirectional)
         
     def forward(self, input, hidden):
-        embedded = self.embedding(input)
-        return self.rnn(embedded, hidden)
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.rnn(output, hidden)
+        return output, hidden
 
     def initHidden(self):
-        return torch.zeros((2 if self.bidirectional else 1) * self.num_layers, 1, self.hidden_size, device=self.device)
+        return torch.zeros((2 if self.bidirectional else 1), 1, self.hidden_size, device=self.device)
         
     def initEncoderHidden(self):
         if self.rnn_type == 'lstm':
